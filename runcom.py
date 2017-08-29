695dd38ea318ef3063158e38b146f2995de59e0f
Erik Letson <hmagellan@gmail.com>
2017-08-28 22:03:45 -0500

    Gave runcom.py old functionality of killit.py, and removed the latter

--- runcom.py
+++ runcom.py
@@ -14,7 +14,6 @@ import os, time
 #variables
 compath = os.path.join('data', 'command')
 cmd = ""
-write_token = ""
 
 ################################################################################
 ################################################################################
@@ -22,11 +21,17 @@ write_token = ""
 #function definitions
 
 #function to write to config file
-def write_to_file():
+def write_to_file(write_token):
 
     with open(compath, 'w') as f:
 
         f.write(write_token)
+
+    #delay for the take
+    time.sleep(15)
+
+    #rewrite
+    open(killpath, 'w').close()
 
 ################################################################################
 ################################################################################
@@ -57,11 +62,50 @@ while True:
         print("")
         print("Following are some of the commands that you can issue to the commander:")
         print("")
+        print("  c..........Cancel a pending command")
         print("  h..........Display this help screen")
         print("  k..........Kill ALL running instances of Lottobot immediately")
         print("  n..........Kill ALL running instances of Lottobot at the end of the current lottery")
         print("  q..........Quit the commander")
         print("  t..........Trim (empty) ALL log files")
         print("")
+        print("Most commands that you issue to the commander will take effect only after a time delay. This is to ensure the command is read by Lottobot, since it only reads commands at a certain point in its 'pass' cycle.")
         print("To issue a command to the commander, input a letter corresponding to a command above and press [ENTER].")
         print("")
+
+    #kill lottobot immediately
+    elif cmd == 'k':
+
+        print("")
+        print("==Kill Lottobot (Immediate)==")
+        print("")
+        print("This will kill ALL running instances of Lottobot immediately.")
+
+        answer = input("Are you SURE you want to do this? (y/n) >>>")
+
+        if answer.lower() == 'y':
+
+            #prompt
+            print("")
+            print("Killing lottobot...")
+            print("Please wait appx. 15 seconds...")
+
+            write_to_config("KILL")
+
+            print("")
+            print("Killing complete!")
+            print("")
+
+        else:
+
+            print("OK, Lottobot will continue running.")
+            print("")
+
+    #if the command is illegal
+    else:
+
+        print("Sorry, your command was not understood.")
+        print("For a list of legal commands, press 'h'.")
+        print("")
+
+    cmd = input("Please enter a command >>>")
